56ee917dac4c7dbcec2836c45df2ea5ef61b6e6e
Chris <onetrckchris@gmail.com>
2020-03-01 11:23:16 -0800

    Created a basic Navbar component and installed the styled-components dependency

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+		</div>
+	);
 }
 
 export default App;
---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2>Chris's Portfolio</h2>
+            <nav>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+                <a href="#">Projects</a>
+            </nav>
+        </div>
+    );    
+};
+
+export default Navbar;
