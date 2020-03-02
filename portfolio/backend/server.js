47dd5605cc2d09b21c5e2835fca98a577d2d2cde
Chris <onetrckchris@gmail.com>
2020-03-02 12:31:28 -0800

    Starting creation of backend server

---
+++ portfolio/backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const port = 5000;
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello world from Express!');
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
